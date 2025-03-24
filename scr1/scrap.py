92790e03d866e73d32ae81b3e90d868db6465557
KrishnanditoLksn <krislaksana123@gmail.com>
2025-03-24 09:17:45 +0700

    feat:only climate url

--- scr1/scrap.py
+++ scr1/scrap.py
@@ -2,14 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 all_urls  =[]
-url = "https://edition.cnn.com/search?q=climate+change+&from=0&size=10&page=1&sort=relevance&types=all&section="
+url = "https://www.cnn.com"
 
 data = requests.get(url).text
 soup = BeautifulSoup(data,features="html.parser")
 for a in soup.find_all('a',href=True):
-    if a['href'] and ['href'][0] == '/climate'  and a['href'] != '#':
+    if a['href'] and a['href'][0] == '/' and a['href'] != '#':
         a['href'] = url + a['href']
     all_urls.append(a['href'])
 
 
 print(all_urls)
+
+
+def url_is_article(url):    
+    if url:
+        if 'cnn.com/' in url:
+            if 'climate' in url.lower():
+                return True
+    return False
+
+
+article_urls = [url for url in all_urls if url_is_article(url)]
+print(article_urls)
